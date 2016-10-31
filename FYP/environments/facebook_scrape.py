2e8c11cf5475ec1af01308b17fa12c47b5d864ce
bendtheji <benj.soon@gmail.com>
2016-10-31 15:34:23 +0800

    code to handle "FIND US" instead of "CONTACT INFO"

--- FYP/environments/facebook_scrape.py
+++ FYP/environments/facebook_scrape.py
@@ -3,9 +3,13 @@ import json
 import re
 from bs4 import BeautifulSoup
 
+# TO-DO : 1) Find better way to get "Find US" Section
+#		  2) Get all content from location
+
 PHONE_ICON = "https://static.xx.fbcdn.net/rsrc.php/v3/yD/r/29f4AUE0XdF.png"
 WEBSITE_ICON = "https://static.xx.fbcdn.net/rsrc.php/v3/yE/r/upNlw510Jd7.png"
 MAIL_ICON = "https://static.xx.fbcdn.net/rsrc.php/v3/yg/r/wLlG5cEEIuu.png"
+LOCATION_ICON = "https://static.xx.fbcdn.net/rsrc.php/v3/yF/r/kS8eNysxft5.png"
 
 def determine_field_and_value(current_section):
 	image_tag = current_section.find('img')
@@ -13,7 +17,7 @@ def determine_field_and_value(current_section):
 	key = ""
 	value = ""
 	text_content = image_tag.parent.next_sibling
-
+	print(text_content.renderContents())
 	while(text_content.find('div') != None):
 		text_content = text_content.find('div')
 
@@ -28,21 +32,40 @@ def determine_field_and_value(current_section):
 	elif image_src == MAIL_ICON:
 		key = "Mail"	
 
+	elif image_src == LOCATION_ICON:
+		key = "Location"	
+
 	return key, value	
 
 def search_contact_info_section(contact_info):
+	#print contact_info
+	# NEED TO FIND BETTER WAY TO LOCATE "FIND US" SECTION
+	find_us_title = contact_info[0].find('span')
+	#print find_us_title
 	contact_info_title = contact_info[0].find('div', text = re.compile('CONTACT INFO'))
-	contact_info = {}
-	current_section = contact_info_title
+		
+	retrieved_contact_info = {}
 
-	while current_section.next_sibling != None:
-		current_section = current_section.next_sibling
-		key, value = determine_field_and_value(current_section)
-		contact_info[key] = value
-	print(contact_info)	
+	if find_us_title:
+		current_section = find_us_title.parent
+
+		while current_section.next_sibling != None:
+			current_section = current_section.next_sibling
+			#print current_section
+			key, value = determine_field_and_value(current_section)
+			retrieved_contact_info[key] = value
+
+	if contact_info_title:
+		current_section = contact_info_title
+
+		while current_section.next_sibling != None:
+			current_section = current_section.next_sibling
+			key, value = determine_field_and_value(current_section)
+			retrieved_contact_info[key] = value
+		print(retrieved_contact_info)	
 
 	"""
-	sections = contact_info_title.parent.findAll('div')
+	sections = find_us_title.parent.findAll('div')
 	found_strings = []
 	for section in sections:
 		section_string = str(section)
@@ -51,7 +74,7 @@ def search_contact_info_section(contact_info):
 
 def main():
 	# Change URL to scrape the different page
-	url = "https://www.facebook.com/DeloitteSingapore/about/"
+	url = "https://www.facebook.com/timbreplus/about/"
 
 	web_response = urllib2.urlopen(url)
 	readable_page = web_response.read()
